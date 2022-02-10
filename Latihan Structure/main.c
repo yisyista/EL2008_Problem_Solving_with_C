df1eaea88febf122f9838438a84198dc27d64376
yisyista <kayyisa.firdaus@gmail.com>
2022-02-10 13:03:04 +0700

    Update main.c

--- Latihan Structure/main.c
+++ Latihan Structure/main.c
@@ -7,15 +7,15 @@ struct mhs {
     int kehadiran;
 };
 
-struct mhs data[5] = {{"Mika", 18320111, 70}, {"Alika", 18320888, 85}, {"Nando", 18320959, 60}, {"Mecca", 18320183, 90}, {"Chandra", 18320632, 43}};
+struct mhs data[30] = {{"Mika", 18320111, 70}, {"Alika", 18320888, 85}, {"Nando", 18320959, 60}, {"Mecca", 18320183, 90}, {"Chandra", 18320632, 43}};
 
 int main()
 {
     int i;
     printf("\nMahasiswa dengan kehadiran di bawah 80 persen\n");
-    for(i=0; i<=4; i++){
+    for(i=0; i<5; i++){
         if (data[i].kehadiran < 80){
-            printf("Nama : %s \nNIM : %d\n", data[i].nama, data[i].nim);
+            printf("Nama : %s \nNIM : %d\n\n", data[i].nama, data[i].nim);
 
         }
     }
